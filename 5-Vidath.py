9ed1169ce70bea0de1ee3ee2c19ae4bfd49e2bbc
Vidath19 <111895260+Vidath19@users.noreply.github.com>
2022-09-12 16:26:26 +0700

    Add files via upload

---
+++ 5-Vidath.py
@@ -0,0 +1,17 @@
+def Partition(A,p,r):
+    x=A[r]
+    i=p-1
+    for j in range(p,r):
+        if A[j] <= x:
+            i =i+1
+            A[i], A[j] = A[j], A[i]
+    A[i+1],A[r] = A[r],A[i+1]
+    return i+1;
+def QuickSort(A,p,r):
+    if p<r:
+        q=Partition(A,p,r)
+        QuickSort(A,p,q-1)
+        QuickSort(A,q+1,r)
+A = [15, 9, 11, 2 ,21,12]
+QuickSort(A, 0, 5)
+print(A)
